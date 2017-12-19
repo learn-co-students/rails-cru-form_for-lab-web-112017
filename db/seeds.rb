790e66211f7f0e1fcddc13038d9ce518a63abc97
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-12-19 16:09:56 -0500

    complete rails cru_form_for lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist1 = Artist.create(name: "Kesha", bio: "born into music, songwriter")
+artist2 = Artist.create(name: "Sam Hunt", bio: "grew up in South loving music")
+artist3 = Artist.create(name: "DJ Khaled", bio: "always loved music")
+genre1 = Genre.create(name: "pop")
+genre2 = Genre.create(name: "country")
+genre3 = Genre.create(name: "rap")
+song1 = Song.create(name:"Rainbow", artist_id: 1, genre_id: 1)
+song2 = Song.create(name:"Saturday Night", artist_id: 2, genre_id: 2)
+song3 = Song.create(name:"I'm the one", artist_id: 3, genre_id: 3)
+song4 = Song.create(name:"Timber", artist_id: 1, genre_id: 2)
+song5 = Song.create(name:"Body Like a Back Road", artist_id: 2, genre_id: 1)
+song6 = Song.create(name:"Animal", artist_id: 1, genre_id: 1)
